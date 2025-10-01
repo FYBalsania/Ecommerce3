2eae001d364f18a424ebe8228e117785855f83c4
daanishjapanwala <daanish.japanwala@swiftweb.in>
2025-10-01 19:51:51 +0530

    Admin - Product Attribute UI

--- Ecommerce3.Admin/Controllers/ProductAttributesController.cs
+++ Ecommerce3.Admin/Controllers/ProductAttributesController.cs
@@ -4,9 +4,24 @@ namespace Ecommerce3.Admin.Controllers;
 
 public class ProductAttributesController : Controller
 {
-    // GET
+    [HttpGet]
     public IActionResult Index()
     {
+        ViewData["Title"] = "Product Attributes";
+        return View();
+    }
+    
+    [HttpGet]
+    public IActionResult Add()
+    {
+        ViewData["Title"] = "Add Product Attribute";
+        return View();
+    }
+    
+    [HttpGet]
+    public IActionResult Edit()
+    {
+        ViewData["Title"] = "Edit Product Attribute - Color";
         return View();
     }
 }
