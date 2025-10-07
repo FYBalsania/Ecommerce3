857010b49da1a23442c1ab4943021e1f6335cb9a
faizalyb <faizal_yb@hotmail.com>
2025-10-07 17:32:47 +0530

    WIP

--- Ecommerce3.Infrastructure/DependencyInjection.cs
+++ Ecommerce3.Infrastructure/DependencyInjection.cs
@@ -14,7 +14,8 @@ public static class DependencyInjection
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddDbContext<AppDbContext>(options =>
-            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"))
+                .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking));
         services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
         services.AddScoped<IUnitOfWork, UnitOfWork>();
         services.AddScoped<IBrandRepository, BrandRepository>();
