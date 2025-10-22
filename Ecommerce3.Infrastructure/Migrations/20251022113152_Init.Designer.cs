b7b814c3ad0c754e4b9cd793402c6387c39da6d7
daanishjapanwala <daanish.japanwala@swiftweb.in>
2025-10-22 17:22:34 +0530

    migrations

--- Ecommerce3.Infrastructure/Migrations/20251022113152_Init.Designer.cs
+++ Ecommerce3.Infrastructure/Migrations/20251022113152_Init.Designer.cs
@@ -12,8 +12,8 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Ecommerce3.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20251021130632_SeedData")]
-    partial class SeedData
+    [Migration("20251022113152_Init")]
+    partial class Init
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
