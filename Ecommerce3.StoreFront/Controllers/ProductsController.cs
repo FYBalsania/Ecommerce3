8ea2e727d38117bf03d38523d7403bd313f87ec3
Faizal Balsania <faizalyb@Faizals-MacBook-Air.local>
2026-01-13 16:30:33 +0530

    WIP

--- Ecommerce3.StoreFront/Controllers/ProductsController.cs
+++ Ecommerce3.StoreFront/Controllers/ProductsController.cs
@@ -10,18 +10,20 @@ public class ProductsController(
     IProductQueryRepository productQueryRepository) : Controller
 {
     [ValidateAntiForgeryToken]
-    public async Task<IActionResult> NextPage(int category, int[] brands, decimal? minPrice, decimal? maxPrice,
+    public async Task<IActionResult> GetByParams(int category, int[] brands, decimal? minPrice, decimal? maxPrice,
         IDictionary<int, decimal>? weights, IDictionary<int, int>? attributes, int pageNumber, SortOrder sortOrder,
         CancellationToken cancellationToken)
     {
         weights ??= new Dictionary<int, decimal>();
         attributes ??= new Dictionary<int, int>();
-        
+
         var pageSize = configuration.GetValue<int>("PLPSize");
         var descendantIds = await categoryQueryRepository.GetDescendantIdsAsync(category, cancellationToken);
         var pagedResult = await productQueryRepository.GetListItemsAsync(descendantIds, brands,
             minPrice, maxPrice, weights, attributes, sortOrder, pageNumber, pageSize, cancellationToken);
-        
-        return PartialView("_PLPProductListItemsPartial", pagedResult);
+
+        return pageNumber == 1
+            ? PartialView("_PLPProductListPartial", pagedResult)
+            : PartialView("_PLPProductListItemsPartial", pagedResult.Data);
     }
 }
--- Ecommerce3.StoreFront/wwwroot/js/products.js
+++ Ecommerce3.StoreFront/wwwroot/js/products.js
@@ -7,18 +7,24 @@ if (document.readyState !== "loading") {
 
 function document_ready() {
     document.getElementById("load_more").addEventListener("click", load_more_clicked);
+    document.getElementById("sort_order").addEventListener("change", sort_order_changed);
 }
 
-async function load_more_clicked(){
+async function sort_order_changed(event) {
+    await get_products_by_params(1);
+}
+
+async function get_products_by_params(pageNumber) {
     //Get values from html elements.
     const categoryId = document.getElementById("category").dataset.id;
     const selectedBrandIds = Array.from(
         document.querySelectorAll('input[type="checkbox"][data-brand-id]:checked')
     ).map(cb => Number(cb.dataset.brandId));
-    const pageNumber = document.getElementById("page_number").value;
+    const totalProducts = Number(document.getElementById("products_count").value);
+    const pageSize = Number(document.getElementById("page_size").value);
     const sortOrder = document.getElementById("sort_order").value;
     const requestVerificationToken = document.querySelector('input[name="__RequestVerificationToken"]').value;
-    
+
     //Construct url query string.
     const params = new URLSearchParams();
     params.append("category", categoryId);
@@ -27,13 +33,13 @@ async function load_more_clicked(){
     params.append("maxPrice", null);
     params.append("weights", null);
     params.append("attributes", null);
-    params.append("pageNumber", (Number(pageNumber) + 1).toString());
+    params.append("pageNumber", (pageNumber).toString());
     params.append("sortOrder", sortOrder);
 
     //Construct url.
-    const url = new URL("/Products/NextPage", window.location.origin);
+    const url = new URL("/Products/GetByParams", window.location.origin);
     url.search = params.toString();
-    
+
     //Call endpoint.
     const response = await fetch(url, {
         method: "GET",
@@ -41,13 +47,29 @@ async function load_more_clicked(){
             "RequestVerificationToken": requestVerificationToken
         }
     });
-    
+
     //Process response.
     if (!response.ok) {
-        alert("Error occured while loading products.");
-    }
-    else {
+        alert("Error occured while loading products. Please try again.");
+    } else {
         const html = await response.text();
-        document.getElementById("product_list").innerHTML += html;
+        if (pageNumber === 1) {
+            document.getElementById("product_list").outerHTML = html;
+        } else {
+            document.getElementById("product_list").innerHTML += html;
+            //Update page number.
+            document.getElementById("page_number").value = (pageNumber + 1).toString();
+        }
+
+        //Toggle load more button's visibility.
+        if (totalProducts <= (pageNumber + 1) * pageSize)
+            document.getElementById("load_more_container").style.display = "none";
+        else
+            document.getElementById("load_more_container").style.display = "block";
     }
+}
+
+async function load_more_clicked() {
+    const pageNumber = Number(document.getElementById("page_number").value);
+    await get_products_by_params(pageNumber + 1);
 }
